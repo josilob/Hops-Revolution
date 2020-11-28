e1068cb5ccd2302d5d33dd642671c593cbc814de
josilob <bjosilo@gmail.com>
2020-11-27 19:40:24 -0500

    home page, style, backgrounds, color palette

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,10 @@ import './Home.css';
 function Home() {
 	return (
 		<div className='home'>
-			<h2 className='toast'>
-				“Of all my favorite things to do, <br />
-				The utmost is to have a brew. <br />
-				My love grows for my foamy friend, <br />
-				With each thirst-quenching elbow bend.
-				<br />
-				Beer’s so frothy, smooth and cold <br />
-				It’s paradise–pure liquid gold."
-			</h2>
+			<img src='hops-home.png' alt='home' className='home-hops' />
+			<div className='welcome-container'>
+				<h2 className='welcome'>Crafted to inspire</h2>
+			</div>
 		</div>
 	);
 }
