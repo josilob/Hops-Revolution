d9ebad888544172ffd34cea6ef9c41de4d2f1859
josilob <bjosilo@gmail.com>
2020-11-29 22:20:51 -0500

    responsiveness

--- src/components/BeerDetails/BeerDetails.js
+++ src/components/BeerDetails/BeerDetails.js
@@ -5,20 +5,16 @@ function BeerDetails({ data, clearDetails }) {
 	return (
 		<div className='beer-details'>
 			<div className='beer-description'>
-				{data.name}
-				<br />
-				Brewery : {data.breweries[0].name}
-				<br />
-				Place : {data.breweries[0].locations[0].locality}
-				<br />
-				Style : {data.style?.name}
-				<br />
-				ABV : {data.abv}
-				<br />
-				IBU : {data.ibu}
-				<br />
-				{data.description}
-				<br />
+				<p className='beer-api-data'>{data.name}</p>
+				<p className='beer-api-data'>Brewery : {data.breweries[0].name}</p>
+
+				<p className='beer-api-data'>
+					Place :{data.breweries[0].locations[0].locality}
+				</p>
+				<p className='beer-api-data'>Style : {data.style?.name}</p>
+				<p className='beer-api-data'>ABV : {data.abv}</p>
+				<p className='beer-api-data'>IBU : {data.ibu}</p>
+				<p className='beer-api-data'>{data.description}</p>
 				<br />
 				<button className='details-btn' onClick={() => clearDetails()}>
 					close
