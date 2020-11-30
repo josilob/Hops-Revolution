6e7dce0c7585d466374c6b18c129ebb636c02bbc
josilob <bjosilo@gmail.com>
2020-11-29 23:28:01 -0500

    pre-brewery lookup on click

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,22 +8,17 @@ function About() {
 				<img className='ga-logo' src='ga-logo.png' alt='img' />
 				<p className='about-text'>
 					As an emerging web developer, with a special interest in frontend
-					frameworks like ReactJS and its integration with the backend code,
-					I've built this page utilizing OpenBreweryDB and BreweryDB APIs,
-					together with google maps for reversed geolocation and Styled
-					components.
+					frameworks like ReactJS, I've built this page utilizing OpenBreweryDB
+					and BreweryDB APIs, together with google maps for reversed geolocation
+					and Styled components.
 				</p>
 				<br />
-				<p className='about-text'>
-					- GA Capstone Project, reflection of a 12 week long software
-					engineering, immersive bootcamp -
-				</p>
+
 				<br />
 				<p className='about-text'>
 					Main functionality is based around being able to look up beers across
 					the country, finding more information about each, and the happy place
-					of their creation, located on the map with aditional contact
-					information.
+					of their creation, with aditional contact informations.
 				</p>
 			</div>
 		</div>
